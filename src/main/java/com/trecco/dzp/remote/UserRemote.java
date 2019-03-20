92b49359f43fca9a1b9bb3dbc7f4dc78b2719b7e
dzp <614495906@qq.com>
2019-03-20 13:50:03 +0800

    添加注释

--- src/main/java/com/trecco/dzp/remote/UserRemote.java
+++ src/main/java/com/trecco/dzp/remote/UserRemote.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name= "spring-cloud-producer", fallback = UserRemoteHystrix.class)
 public interface UserRemote {
+    //这里需要和服务提供者的访问地址一致
     @RequestMapping(value = "/findByIdCard")
     public String findUserByIdCard(@RequestParam(value = "idCard") String idCard);
 }
